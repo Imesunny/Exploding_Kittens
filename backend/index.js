37ec7b84a5580159ee368e1a39a505e43ac08bc8
Sunny Anand <rvrahulvermaonly1@gmail.com>
2024-03-16 08:01:32 +0530

    Writing routes for the backend of the game

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { UserModel } = require('./modules/User.model');
 const app = express();
+app.use(cors());
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.json({message: "Hey, you are on the Home Page!"})
+})
+
+app.post('/api/user', async(req, res)=>{
+    const{username} = req.body;
+    const user = new UserModel({
+        username, 
+        points: 0
+    })
+    await user.save();
+    res.send('User points initialized successfully')
 })
 
 app.listen(8080,()=>{
